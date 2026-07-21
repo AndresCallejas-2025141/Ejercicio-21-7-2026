fa5ea6be93bc8c86cdc8a7d25a068129f2030482
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-07-21 11:19:21 -0600

    add: Se agregaron los metodos de Clientes en server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import http, { IncomingMessage, ServerResponse } from "http";
 import { obtenerProductos, obtenerProductoPorId, agregarProducto, actualizarProducto, eliminarProducto } from "./controllers/productoController";
+import { obtenerClientes, obtenerClientePorId, agregarCliente, actualizarCliente, eliminarCliente } from "./controllers/clienteController";
 
 export const server = http.createServer(
     (req: IncomingMessage, res: ServerResponse) => {
@@ -47,6 +48,52 @@ export const server = http.createServer(
         const id = Number(partes[2]);
 
         eliminarProducto(id, res);
+
+        return;
+
+        }
+
+        if (req.url === "/clientes" && req.method === "GET") {
+
+        obtenerClientes(res);
+
+        return;
+
+        }
+
+        if (req.method === "GET" && req.url?.startsWith("/clientes/")) {
+
+        const id = Number(req.url.split("/")[2]);
+
+        obtenerClientePorId(id, res);
+
+        return;
+
+        }
+
+        if (req.url === "/clientes" && req.method === "POST") {
+
+        agregarCliente(req, res);
+
+        return;
+
+        }
+
+        if (req.method === "PUT" && req.url?.startsWith("/clientes/")) {
+
+        const id = Number(req.url.split("/")[2]);
+
+        actualizarCliente(req, res, id);
+
+        return;
+
+        }
+
+        if (req.method === "DELETE" && req.url?.startsWith("/clientes/")) {
+
+        const id = Number(req.url.split("/")[2]);
+
+        eliminarCliente(id, res);
 
         return;
 
